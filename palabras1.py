27457ee76c3079c65f41fe4ac59c26e9742d199b
polancodf2024 <polanco@unam.mx>
2026-04-06 22:04:16 -0600

    Add files via upload

--- palabras1.py
+++ palabras1.py
@@ -2041,7 +2041,8 @@ def main():
         st.info("🆕 **Nueva Búsqueda**")
         
         query = st.text_area("**PubMed search strategy:**",
-            value="(\"myocardial infarction\"[Mesh] OR \"myocardial infarction\"[tiab]) AND (\"intramyocardial dissection\"[tiab] OR \"intramyocardial dissecting hematoma\"[tiab] OR \"intramyocardial dissecting\"[tiab])"
+            consulta_pubmed = r'("myocardial infarction"[Mesh] OR "myocardial infarction"[tiab]) AND ("intramyocardial dissection"[tiab] OR "intramyocardial dissecting hematoma"[tiab] OR "intramyocardial dissecting"[tiab])'
+            value = consulta_pubmed
             height=100)
         
         threshold = st.slider("Relevance threshold:", 0.0, 0.9, 0.35, 0.05)
